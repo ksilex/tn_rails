e69d147298fa9d67f48de949f9bd71fc7d4c114a
ksilex <negative074@gmail.com>
2020-11-30 20:06:42 +0300

    first task

---
+++ equation.rb
@@ -0,0 +1,21 @@
+puts "Введите a"
+a = gets.chomp.to_f
+
+puts "Введите b"
+b = gets.chomp.to_f
+
+puts "Введите c"
+c = gets.chomp.to_f
+
+d = b**2 - 4*a*c
+
+if d<0
+    puts "Уравнение не имеет корней"
+elsif d==0
+    x1 = -b / (2*a)
+    puts "x1 и х2 равны: #{x1}"
+else
+    x1 = (-b + Math.sqrt(d)) / (2*a)
+    x2 = (-b + -Math.sqrt(d)) / (2*a)
+    puts "Первый корень равен: #{x1}\nВторой корень равен:#{x2}"
+end
---
+++ weight.rb
@@ -0,0 +1,13 @@
+puts "Ваше имя"
+name = gets.chomp
+
+puts "Ваш рост"
+height = gets.chomp.to_i
+
+coef = (height - 110) * 1.15
+
+if coef < 0 
+    puts "Ваш вес уже оптимальный"
+else
+    puts "#{name}, ваш идеальный вес равен #{coef.round(2)}"
+end
